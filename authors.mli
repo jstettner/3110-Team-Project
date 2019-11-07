13d0000a6897aac7523d2364f167f92f27068c7f
Jack Stettner <jack@stet.com>
2019-11-06 19:15:52 -0500

    submission criteria

---
+++ authors.mli
@@ -0,0 +1,7 @@
+(**
+   Metadata about this submission.
+   @author Jack Stettner, jws397, Sam Brickman, sab389, Alina Kim, ak778. 
+*)
+
+(** [hours_worked] is the number of hours you worked on this assignment. *)
+val hours_worked : int list
