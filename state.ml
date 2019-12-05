994684877fd8d07273c2305c117f593e180bdd6e
ak778 <ak778@cornell.edu>
2019-12-04 21:44:25 -0500

    started documenting state.ml

--- state.ml
+++ state.ml
@@ -1,10 +1,12 @@
 open Deck
 open Player
 
+(** [bet] is the type of *)
 type bet = int
 type bust = bool
 type hard = bool
 
+(** [t] is the type of state. *)
 type t = {
   players: (Player.t * bet * bust * hard) list;
   deck: Deck.t;
@@ -14,6 +16,8 @@ type t = {
 
 let initial_cash = 500
 
+(** [symbol_of_suit s] is the string that prints the ASCII symbol matching 
+    suit [s]. *)
 let symbol_of_suit (s : suit) : string = 
   match s with 
   | Heart -> "    ♥    "
@@ -21,6 +25,9 @@ let symbol_of_suit (s : suit) : string =
   | Spade -> "    ♠    "
   | Club -> "    ♣    "
 
+(** [left_face_of_value] is the string that prints the appropriate number or
+    face card letter corresponding to card value [v] with the alphanumeric
+    character aligned left. *)
 let left_face_of_value (v : int) : string =
   if v = 11 then "J        "
   else if v = 12 then "Q        "
@@ -29,6 +36,9 @@ let left_face_of_value (v : int) : string =
   else if v = 10 then "10       "
   else string_of_int v ^ "        "
 
+(** [right_face_of_value] is the string that prints the appropriate number or
+    face card letter corresponding to card value [v] with the alphanumeric 
+    character aligned right. *)
 let right_face_of_value (v : int) : string =
   if v = 11 then "        J"
   else if v = 12 then "        Q"
@@ -37,6 +47,7 @@ let right_face_of_value (v : int) : string =
   else if v = 10 then "       10"
   else "        " ^ string_of_int v 
 
+(** [print_card st card i] prints line [i] of card [card] to terminal. *)
 let print_card (st : t) (card : card) (i : int) : unit =
   if i = 0 then print_string "┌─────────┐"
   else if i = 1 then print_string ("│" ^ left_face_of_value card.value ^ "│")
@@ -45,6 +56,7 @@ let print_card (st : t) (card : card) (i : int) : unit =
   else if i = 8 then print_string "└─────────┘"
   else print_string "│         │"
 
+(** [print_hidden_card x] prints a face down playing card to terminal. *)
 let print_hidden_card x: unit =
   begin 
     print_endline "┌─────────┐";
@@ -58,23 +70,29 @@ let print_hidden_card x: unit =
     print_endline "└─────────┘";
   end
 
+(** [print_hand st hand i] prints line [i] of each card in [hand]. *)
 let rec print_hand (st : t) (hand : card list) (i : int) : unit =
   ignore (List.map (fun x -> print_card st x i) hand); print_endline ""
 
+(** [get_player st p_id] is player object corresponding to player_id [p_id] 
+    with the player's current bet amount, whether they busted or not, and 
+    whether they have a soft or hard hand. *)
 let get_player (st : t) (p_id: player_id) : Player.t * bet * bust * hard =
   let found = List.find (fun (x, _, _, _) -> x.id = p_id) st.players in
   found
 
+(** [print st id] prints each card in the hand of the player corresponding to
+    player_id [id]. *)
 let print (st : t) (id: player_id) : unit =
   let (player, _, _, _) = get_player st id in
   let hand = get_hand player in
   for j = 0 to 8 do
     (print_hand st hand j) done;;
 
+(** [print_dealer st] prints each card in the dealer's hand to terminal. *)
 let print_dealer (st : t) : unit =
   let (dealer, _, _) = st.house in
   let hand = get_hand dealer in
-  (* ignore (List.map (fun x -> print_endline ((string_of_int x.value))) hand); *)
   match hand with
   | h :: t -> begin print_hidden_card 0; 
       for j = 0 to 8 do
@@ -83,7 +101,8 @@ let print_dealer (st : t) : unit =
 
 let rec generate_n_players (players : Player.t list) = function
   | 0 -> players
-  | n -> generate_n_players (Player.new_player n initial_cash :: players) (n - 1)
+  | n -> generate_n_players (Player.new_player n initial_cash :: players) 
+           (n - 1)
 
 let new_game (player_num : int) (shuffle_amt : int) : t = 
   let players = generate_n_players [] player_num in
