ff994ad2861495692274809608991ede6a95f2ce
Jack Stettner <jack@stet.com>
2019-12-05 22:57:53 -0500

    added one more option for double down input

--- command.ml
+++ command.ml
@@ -9,6 +9,6 @@ let parse str =
   match str with 
   | "Hit" | "hit" | "h" -> Hit
   | "Stand" | "stand" | "s" -> Stand
-  | "Double Down" | "double down" | "Double" | "Down" | "double" | "down" -> 
+  | "Double Down" | "double down" | "Double" | "Down" | "double" | "down" | "d" -> 
     Double
   | _ -> raise Malformed
